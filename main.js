962d2c6a66716da1ca6fc2afb03b94db34103010
Courtney McRae <courtneymcrae9@gmail.com>
2015-12-01 13:15:04 -0800

    builds sequence for questions

--- main.js
+++ main.js
@@ -1,42 +1,116 @@
+var question1 = {
+		question : "What does the C in CSS stand for?",
+		answer1: "Console",
+		answer2: "Cascading",
+		answer3: "Catapult", 
+		answer4: "Courtney",
+		correct_answer: "Cascading"
+	}; 
 
+var question2 = {
+		question : "What does the H in HTML stand for?",
+		answer1: "Handle",
+		answer2: "High",
+		answer3: "Hanging", 
+		answer4: "Hyper",
+		correct_answer: "Hyper"
+
+	};
+
+var question3 = {
+		question : "How do you declare a variable in JavaScript?",
+		answer1: "var",
+		answer2: "function",
+		answer3: "if", 
+		answer4: "for",
+		correct_answer: "var"
+
+	};
 
 var list = [
-	question1 = {
-		question : "who was the first president",
-		answer1: "washington",
-		answer2: "lincoln",
-		answer3: "george W", 
-		answer4: "obama",
-		correct_answer: 1
-	}, 
-	question2 = {
-		question : "who was the second president",
-		answer1: "washington",
-		answer2: "lincoln",
-		answer3: "george W", 
-		answer4: "obama",
-		correct_answer: 2
+	question1,
+	question2,
+	question3
+];
 
-	}, 
-	question3 ={
-		question : "who was the third president",
-		answer1: "washington",
-		answer2: "lincoln",
-		answer3: "george W", 
-		answer4: "obama",
-		correct_answer: 3
+ var currentQuestion = 0;
+ var playerScore = 0; 
 
-	}
 
-]
+function play (q) {
+	$('#question').html(list[q].question)
+	$('#answer1').html(list[q].answer1)
+	$('#answer2').html(list[q].answer2)
+	$('#answer3').html(list[q].answer3)
+	$('#answer4').html(list[q].answer4)
 
-function courtney () {
-	$('#question').html(list[0].question)
-	$('#answer1').html(list[0].answer1)
-	$('#answer2').html(list[0].answer2)
-	$('#answer3').html(list[0].answer3)
-	$('#answer4').html(list[0].answer4)
 }
 
-// rename function courtney so that it has an appropriate naming convention
+
+
+// function answer (a) {
+// 	console.log(a.html)
+// }
+
+
+
+$(".answers").on("click", function (a) {
+	console.log(a)
+
+	if (this.innerHTML === list[currentQuestion].correct_answer){
+		console.log("correct")
+		this.innerHTML = "CORRECT!";
+		// do code here that adds a correct answer to the score
+		playerScore ++;
+		$('#score').html(playerScore)
+		
+	} else {
+		console.log("wrong answer")
+		this.innerHTML = "WRONG ANSWER!"
+	}
+})
+
+
+
+
+$("#play").on("click", function(){
+	console.log(currentQuestion)
+	play(currentQuestion)
+})
+
+$('#next').on("click", function(){
+	currentQuestion ++
+	play(currentQuestion)
+})
+
+
+// $("#answer1").on("click", function () {
+//     console.log( "correct" ) 
+// })
+// $("#answer2").on("click", function () {
+//     console.log( "incorrect" )
+// })
+// $("#answer3").on("click", function () {
+//     console.log( "incorrect" ) 
+// })
+// $("#answer4").on("click", function () {
+//     console.log( "incorrect" ) 
+// })
+
+
+
+// $( "answer2" ).click({
+//     play function() {
+//         console.log( "incorrect" );
+//     },
+//     $("answer3"): function() {
+//         console.log( "incorrect" );
+//     },
+//     click: function() {
+//         console.log( "correct" );
+//     }
+// });
+
+
+
 // figure out how to call different questions, and their appropriate answers, either randomly, or just in order, as long as it works its way through "list" (list of questions (the array of question object literals))
